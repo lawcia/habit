492dc957829d73dd4abfe66a43e0298754c3e83d
lawcia <lawrencianjume@gmail.com>
2020-01-30 19:43:45 +0000

    run two ports

---
+++ client/src/components/RegisterForm.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Axios from 'axios';
+
+export default class RegisterForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        Axios.post('/api/v1/users/register', this.state)
+            .then((res) => {
+                if(res.data.success){
+                alert(res.data.message)
+                }
+            })
+            .catch((err) => {
+                alert(err.response.data.message)
+                }
+            )
+        event.preventDefault();
+       
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} className="landingForm">
+                <label>
+                    Username:
+            <input type="text" name='username' value={this.state.value} onChange={this.handleChange} minLength={3} maxLength={20} required />
+                </label>
+                {}
+                <br />
+                <label>
+                    Password:
+            <input type="password" name='password' value={this.state.value} onChange={this.handleChange} minLength={6} maxLength={15} required />
+                </label>
+                <br />
+                <input className="submitBtn" type="submit" value="Register" />
+            </form>
+        );
+    }
+}
---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const session = require('express-session');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const routes = require('./backend/routes/index');
+const bodyParser = require('body-parser');
+const { handleError } = require('./backend/helpers/errors');
+
+require('dotenv').config();
+
+const app = express();
+const {
+    PORT = 8000,
+    NODE_ENV = 'development',
+    SESS_NAME = 'sid',
+    SESS_SECRET
+} = process.env
+
+const TWO_HOURS = 1000 * 60 * 60 * 2;
+app.use(session({
+    name: SESS_NAME,
+    resave: false,
+    saveUninitialized: false,
+    secret: SESS_SECRET,
+    cookie: {
+    maxAge: TWO_HOURS,
+    sameSite: true,
+    secure: false
+    }
+}))
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+const db_name = (process.env.NODE_ENV === 'test' ? 'test_db' : 'habits_db');
+let url = `mongodb://localhost:27017/${db_name}`;
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected!!!'));
+
+app.use('/api/v1', routes);
+
+app.use((req, res, next) => {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err)
+});
+
+app.use((err, req, res, next) => {
+    handleError(err, res)
+});
+
+app.listen(PORT, () => {
+    console.log(`Habit is running on port: ${PORT}`);
+});
+
+module.exports = app;
